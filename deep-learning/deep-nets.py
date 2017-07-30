7f9fe5bfc8d950f62bd8257ebcf5dd0dd60f4b0d
tenzin <ten13zin@gmail.com>
2017-07-30 11:01:54 +0530

    Build Neural Network model with tensorflow

---
+++ deep-learning/deep-nets.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('/tmp/data', one_hot=True)
+
+# about one_hot parameter
+# we have 10 classes, 0-9, with one_hot as true, each data point looks like following
+'''
+0 = [1,0,0,0,0,0,0,0,0,0]
+1 = [0,1,0,0,0,0,0,0,0,0]
+2 = [0,0,1,0,0,0,0,0,0,0]
+3 = [0,0,0,1,0,0,0,0,0,0]
+'''
+
+# hidden layer and their #units
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_hl3 = 500
+
+# output layer and its #units
+n_classes = 10
+batch_size = 100
+
+# matrix is height x width
+X = tf.placeholder('float', [None, 28 * 28])
+y = tf.placeholder('float')
+
+# Nueral network Models
+def neural_networl_model(data):
+
+  # initialization of hidden layers and output layer
+
+  hidden_1_layer = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+                    'biases': tf.Variable(tf.rondom_normal(n_nodes_hl1))}
+
+  hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                    'biases': tf.Variable(tf.rondom_normal(n_nodes_hl2))}
+
+  hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl1])),
+                    'biases': tf.Variable(tf.rondom_normal(n_nodes_hl3))}
+
+  output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+                    'biases': tf.Variable(tf.rondom_normal(n_classes))}
+
+  # Neural network models
+  #(input_data * wieghts) + baises
+
+  l1 = tf.add(tf.multiply(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
+  l1 = tf.nn.relu(l1)
+
+  l2 = tf.add(tf.multiply(l1, hidden_2_layer['weights']) , hidden_2_layer['biases'])
+  l2 = tf.nn.relu(l2)
+
+  l3 = tf.add(tf.multiply(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
+  l3 = tf.nn.relu(l3)
+
+  output = tf.multiply(l3, output_layer['weights']) + output_layer['biases']
+
+  return output
+
+
+
+
+
