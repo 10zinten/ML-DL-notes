04f80f3999e9be2b7699a92af892f9a523125b1f
tenzin <ten13zin@gmail.com>
2017-08-04 18:00:25 +0530

    Cover tensorflow basic

--- deep-learning/tfbasic.py
+++ deep-learning/tfbasic.py
@@ -31,3 +31,37 @@ b = tf.Variable([-.3], dtype=tf.float32)
 x = tf.placeholder(tf.float32)
 linear_model = W * x + b
 
+
+# Another basic Tutorial
+
+# first, create a TensorFlow constant
+const = tf.constant(2.0, name="const")
+
+# create TensorFlow variables
+b = tf.Variable(2.0, name='b')
+c = tf.Variable(1.0, name='c')
+
+# now create some operations
+d = tf.add(b, c, name='d')
+e = tf.add(c, const, name='e')
+a = tf.multiply(d, e, name='a')
+
+# all the constants, variables, operations and the computational graph are only created when the initialisation
+# commands are run.
+# setup the variable initialisation
+init_op = tf.global_variables_initializer()
+
+# The TensorFlow session is an object where all operations are run.
+# start the session
+with tf.Session() as sess:
+    # initialise the variables
+    sess.run(init_op)
+    # compute the output of the graph
+    a_out = sess.run(a)
+    print("Variable a is {}".format(a_out))
+
+
+# create TensorFlow variables, when not shure about the input data
+b = tf.placeholder(tf.float32, [None, 1], name='b') #we can inject as much 1-dimensional data that we want into the b variable of float32
+
+
