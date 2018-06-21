14748cd6829a491c7872b07a8d8945e63533f92d
tenzin <ten13zin@gmail.com>
2018-06-21 13:36:11 +0530

    Compare square error and Huber Error with Linear Regression

--- tf_tuto/stanford-cs20si/lecture-notes/03_lecture/01_linreg_placeholder.py
+++ tf_tuto/stanford-cs20si/lecture-notes/03_lecture/01_linreg_placeholder.py
@@ -27,15 +27,25 @@ Y_predicted = w * X + b
 # Step 5: use the squared error as the loss function
 # Mean squared error
 MSE_loss = tf.square(Y - Y_predicted, name='loss')
+
 # Huber error
-Hubre_loss = utils.huber_loss(Y, Y_predicted)
+def huber_loss(labels, predictions, delta=14.0):  # found in utils.py
+    residual = tf.abs(labels - predictions)
+    def f1(): return 0.5 * tf.square(residual)
+    def f2(): return delta * residual - 0.5 * tf.square(delta)
+    return tf.cond(residual < delta, f1, f2)
+
+Huber_loss = huber_loss(Y, Y_predicted)
 
 # Step 6: Use GD t minimize the loss
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(MSE_loss)
+MSE_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(MSE_loss)
+Huber_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(Huber_loss)
 
+## Training with MSE loss
 
+print("[INFO] Training with MSE loss.....")
 start = time.time()
-writer = tf.summary.FileWriter('./graphs/linear_reg', tf.get_default_graph())
+writer = tf.summary.FileWriter('./graphs/linear_reg_MSE', tf.get_default_graph())
 with tf.Session() as sess:
     # Step 7: initialze the necessary variables, in this case W and b
     sess.run(tf.global_variables_initializer())
@@ -45,7 +55,7 @@ with tf.Session() as sess:
         total_loss = 0
         for x, y in data:
             # Session execute optimizer and fetch value of loss
-            _, loss = sess.run([optimizer, MSE_loss], feed_dict={X: x, Y: y})
+            _, loss = sess.run([MSE_optimizer, MSE_loss], feed_dict={X: x, Y: y})
             total_loss += loss
 
         if i % 10 == 0:
@@ -55,12 +65,44 @@ with tf.Session() as sess:
     writer.close()
 
     # Step 9: output the value of W and b
-    w_out, b_out = sess.run([w, b])
+    MSE_w_out, MSE_b_out = sess.run([w, b])
 
-print('Took: %f seconds' % (time.time() - start))
+print('[INFO] Training Time: %f seconds' % (time.time() - start))
+print()
+
+## Training with Huber loss
+print("[INFO] Training with Huber loss")
+
+data, n_samples = utils.read_birth_life_data(DATA_FILE)
+
+start = time.time()
+writer = tf.summary.FileWriter('./graphs/linear_reg_Huber', tf.get_default_graph())
+with tf.Session() as sess:
+    # Step 7: initialze the necessary variables, in this case W and b
+    sess.run(tf.global_variables_initializer())
+
+    # Step 8: Train the model for 100 epochs
+    for i in range(100):
+        total_loss = 0
+        for x, y in data:
+            # Session execute optimizer and fetch value of loss
+            _, loss = sess.run([Huber_optimizer, Huber_loss], feed_dict={X: x, Y: y})
+            total_loss += loss
+
+        if i % 10 == 0:
+            print('Epoch {0}: {1}'.format(i, total_loss/n_samples))
+
+    # close the writer when done
+    writer.close()
+
+    # Step 9: output the value of W and b
+    Huber_w_out, Huber_b_out = sess.run([w, b])
+
+print('[INFO] Training Time: %f seconds' % (time.time() - start))
 
 # plot the results
 plt.plot(data[:, 0], data[:, 1], 'bo', label='Real data')
-plt.plot(data[:, 0], data[:, 0] * w_out + b_out, 'r', label='Predicted data')
+plt.plot(data[:, 0], data[:, 0] * MSE_w_out + MSE_b_out, 'r', label='Predicted data with square error')
+plt.plot(data[:, 0], data[:, 0] * Huber_w_out + Huber_b_out, c='g', label='Predicted data with Huber loss')
 plt.legend()
 plt.show()
